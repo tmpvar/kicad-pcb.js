2d5f9157ddc1c2f42026d59cbb9a2b1c2e825ce1
Elijah Insua <tmpvar@gmail.com>
2014-11-15 18:41:11 -0800

    implement parser

---
+++ parser.js
@@ -0,0 +1,78 @@
+var through = require('through');
+
+module.exports = createKiCadParserStream;
+
+function createKiCadParserStream(debug) {
+
+  var cmd;
+  var loc;
+  var stack = [];
+  var sawSpace = false;
+  var inQuotes = false;
+  var current = '';
+
+  function addCurrentToken() {
+    var trimmed = current.trim();
+
+    if (!sawSpace){
+      sawSpace = true;
+      loc[0] = current.trim();
+    } else if (trimmed) {
+      loc[1].push(trimmed);
+    }
+    current = '';
+  }
+
+  function handleChunk(chunk) {
+    var str = chunk.toString();
+    var l = str.length;
+
+    for (var i = 0; i<l; i++) {
+      var c = str[i];
+
+      debug && console.log(c, loc, 'stack:', stack);
+
+      if (c === '"') {
+        inQuotes = !inQuotes;
+        continue;
+      }
+
+      if (c === '(' && !inQuotes) {
+        sawSpace = false;
+
+        if (!cmd) {
+          cmd = ['', []];
+          loc = cmd;
+        } else {
+          var newloc = ['', []];
+          loc[1].push(newloc);
+          loc = newloc;
+        }
+
+        stack.push(loc);
+      } else if (c === ')' && !inQuotes) {
+        addCurrentToken();
+        var last = stack.pop();
+
+        debug && console.log('emit', last);
+        this.emit(last[0], last[1]);
+
+        loc = stack[stack.length-1];
+
+        debug && console.log('new loc', loc, stack);
+
+        if (!stack.length) {
+          if (this.listeners('data').length) {
+            this.push(cmd);
+          }
+        }
+      } else if (c === ' ' && !inQuotes) {
+        addCurrentToken();
+      } else {
+        current += c;
+      }
+    }
+  }
+
+  return through(handleChunk);
+}
--- test/test.js
+++ test/test.js
@@ -1,36 +1,20 @@
 var fs = require('fs');
 var path = require('path');
 var test = require('tape');
-var through = require('through2');
+var through = require('through');
 
-var createParseStream = require('../kicad-pcb');
+var createParseStream = require('../parser');
 
 test('basic parse', function(t) {
   var st = through();
+  t.plan(1);
   st.pipe(createParseStream())
     .on('data', function(obj) {
-      t.deepEqual(obj, [ [ 'cmd1' ], ['arg1', 'arg2' ]])
+      t.deepEqual(obj, ['cmd1', ['arg1', 'arg2' ]]);
     })
-    .on('end', t.end.bind(t));
+    .on('end', t.end);
 
   st.end('(cmd1 arg1 arg2)');
-});
-
-test('nested parse', function(t) {
-  var st = through();
-  var expect = [
-    [['top', 'next', 'bottom'], ['abc']],
-    [['top', 'next'], []],
-    [['top'], []],
-  ];
-
-  st.pipe(createParseStream())
-    .on('data', function(obj) {
-      t.deepEqual(obj, expect.shift());
-    })
-    .on('end', t.end.bind(t));
-
-  st.end('(top (next (bottom abc)))');
 });
 
 test('nested parse', function(t) {
@@ -39,8 +23,28 @@ test('nested parse', function(t) {
   st.pipe(createParseStream())
     .on('data', function(obj) {
       t.deepEqual(
+        obj, [
+          'top', [
+            ['next', [
+              ['bottom', ['abc']]
+            ]
+          ]
+        ]
+      ]);
+    })
+    .on('end', t.end.bind(t));
+
+  st.end('(top (next (bottom abc)))');
+});
+
+test('quoted parse', function(t) {
+  var st = through();
+
+  st.pipe(createParseStream())
+    .on('data', function(obj) {
+      t.deepEqual(
         obj,
-        [['quoted'], ['a b', 'c d']]
+        ['quoted', ['a b', 'c d']]
       );
     })
     .on('end', t.end.bind(t));
@@ -48,12 +52,71 @@ test('nested parse', function(t) {
   st.end('(quoted "a b" "c d")');
 });
 
-// test('parser', function(t) {
-//   var s = through();
-//   fs.createReadStream(path.join(__dirname, 'BLDC_4.kicad_pcb'))
-//     .pipe(createParseStream())
-//     .on('data', function(obj) {
-//       console.log(obj);
-//     })
-//     .on('end', t.end.bind(t));
-// });
+test('quoted parse (nested paran', function(t) {
+  var st = through();
+
+  st.pipe(createParseStream())
+    .on('data', function(obj) {
+      t.deepEqual(
+        obj,
+        ['quoted', ['a ( b', 'c ) d']]
+      );
+    })
+    .on('end', t.end.bind(t));
+
+  st.end('(quoted "a ( b" "c ) d")');
+});
+
+
+test('nested with same path', function(t) {
+  var st = through();
+
+  var expect = [
+    [['board', 'child'], ['a', 'b']],
+    [['board', 'child'], ['c', 'd']],
+    [['board'], ['test']],
+  ];
+
+
+  st.pipe(createParseStream())
+    .on('data', function(obj) {
+      t.deepEqual(
+        obj, [
+          'board', [
+            'test',
+            ['child', ['a', 'b']],
+            ['child', ['c', 'd']]
+          ]
+        ]
+      );
+    })
+    .on('end', t.end.bind(t));
+
+  st.end('(board test (child a b) (child c d))');
+});
+
+
+test('emit events', function(t) {
+  var st = through();
+  var lines = 0;
+  var boards = 0;
+
+  st.pipe(createParseStream())
+    .on('end', function() {
+      t.equal(lines, 2, 'should have 2 lines');
+      t.equal(boards, 1, 'should have 1 board');
+      t.end();
+    })
+    .on('line', function(line) {
+      t.equal(line.length, 4);
+      lines++;
+    })
+    .on('board', function(board) {
+      t.equal(board.length, 2);
+      boards++;
+    });
+
+    st.end(
+      '(board (line 10 10 100 100) (line 1 1 2 2))'
+    );
+});
